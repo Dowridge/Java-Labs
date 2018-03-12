651ffa248454bf8bd1893dcda17222b678de0713
Malique Dowridge <robdude55@hotmail.com>
2018-03-12 15:32:47 -0400

    Add files via upload

---
+++ NetworkResolverDemo.java
@@ -0,0 +1,26 @@
+import java.net.*;
+public class NetworkResolverDemo 
+{
+ public static void main(String args[]) 
+ {
+  if (args.length != 1) 
+  {
+   System.err.println("Syntax - NetworkResolverDemo host");
+   System.exit(0);
+  }
+  System.out.println("Resolving " + args[0]);
+  
+  try 
+  {
+   InetAddress addr = InetAddress.getByName(args[0]);
+   System.out.println("IP address : " + addr.getHostAddress());
+   System.out.println("Hostname : " + addr.getHostName());
+  } 
+  
+  catch (UnknownHostException uhe) 
+  {
+   System.out.println("Error - unable to resolve hostname");
+  }
+  
+ }
+}
